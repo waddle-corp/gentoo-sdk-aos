36e980379dd76ca6f7a4989bf2a76b57c3772a6a
nathan.park <dev.nathan.park@gmail.com>
2024-09-02 23:53:38 +0900

    Add send()

--- gentoo/src/main/java/com/waddle/gentoo/internal/api/ApiClient.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/ApiClient.kt
@@ -1,11 +1,17 @@
 package com.waddle.gentoo.internal.api
 
 import com.waddle.gentoo.BuildConfig
+import com.waddle.gentoo.internal.exception.GentooException
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
 
 internal class ApiClient(
+    private val apiKey: String,
     private val baseUrl: String
 ) {
     val okHttpClient: OkHttpClient = OkHttpClient.Builder()
@@ -18,9 +24,39 @@ internal class ApiClient(
         )
         .build()
 
+    fun <T> send(request: ApiRequest, responseHandler: ResponseHandler<T>?) {
+        okHttpClient.newCall(buildRequest(request)).enqueue(
+            object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    responseHandler?.onResponse(
+                        com.waddle.gentoo.internal.util.Response.Failure(
+                            GentooException(e)
+                        )
+                    )
+                }
 
-    fun buildRequest(apiRequest: ApiRequest): Request {
-        val request = Request.Builder() // TODO : add headers here
+                override fun onResponse(call: Call, response: Response) {
+                    if (!response.isSuccessful) {
+                        // TODO add some ErrorResponse and call response handler as failure
+                        return
+                    }
+
+                    val body = response.body
+                    // TODO kotlin serialization
+                }
+            }
+        )
+    }
+
+
+    private fun buildRequest(apiRequest: ApiRequest): Request {
+        val request = Request.Builder()
+            .addHeader("x-api-key", apiKey)
+
+        apiRequest.headers.forEach {
+            request.addHeader(it.key, it.value)
+        }
+
         return when (apiRequest) {
             is GetRequest -> {
                 request.url(baseUrl + apiRequest.getQueryUrl())

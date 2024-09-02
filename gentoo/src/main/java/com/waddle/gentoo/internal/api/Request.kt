b617e2179197f4f9abdf9db0f4d8f4b3e594f770
nathan.park <dev.nathan.park@gmail.com>
2024-09-02 23:17:04 +0900

    Add ApiRequest and children to abstract API Call.

---
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/Request.kt
@@ -0,0 +1,39 @@
+package com.waddle.gentoo.internal.api
+
+import okhttp3.RequestBody
+
+internal sealed interface ApiRequest {
+    val url: String
+}
+
+internal interface GetRequest : ApiRequest {
+    val params: Map<String, String>?
+        get() = null
+
+    fun getQueryUrl(): String {
+        return params?.let { "$url?${it.queryString}" } ?: url
+    }
+}
+
+internal interface PostRequest : ApiRequest {
+    val requestBody: RequestBody
+}
+
+internal interface PutRequest : ApiRequest {
+    val requestBody: RequestBody
+}
+
+internal interface DeleteRequest : ApiRequest {
+    val params: Map<String, String>?
+        get() = null
+
+    val requestBody: RequestBody?
+        get() = null
+
+    fun getQueryUrl(): String {
+        return params?.let { "$url?${it.queryString}" } ?: url
+    }
+}
+
+private val Map<String, String>.queryString: String
+    get() = this.entries.joinToString(separator = "&") { "${it.key}=${it.value}" }

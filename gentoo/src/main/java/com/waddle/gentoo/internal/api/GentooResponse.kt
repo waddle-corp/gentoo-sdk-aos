b7246eb2dfe1b29793c73ce7fcfe68d1868f3a4e
nathan.park <dev.nathan.park@gmail.com>
2024-09-06 22:26:29 +0900

    Remove Exception from GentooResponse

--- gentoo/src/main/java/com/waddle/gentoo/internal/api/GentooResponse.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/GentooResponse.kt
@@ -1,14 +1,10 @@
 package com.waddle.gentoo.internal.api
 
 import com.waddle.gentoo.internal.api.response.ErrorResponse
-import com.waddle.gentoo.internal.exception.GentooException
 
 sealed class GentooResponse<out A> {
     val success: A?
         get() = (this as? Success)?.value
-
-    val exception: GentooException?
-        get() = (this as? Failure)?.e
 
     val error: ErrorResponse?
         get() = (this as? Failure)?.errorResponse
@@ -16,6 +12,5 @@ sealed class GentooResponse<out A> {
     data class Success<out A>(val value: A) : GentooResponse<A>()
     data class Failure(
         val errorResponse: ErrorResponse,
-        val e: GentooException? = null
     ) : GentooResponse<Nothing>()
 }

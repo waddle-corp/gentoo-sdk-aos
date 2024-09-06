e67329f9acbfbd8cb2e02078d49155570fa1297c
nathan.park <dev.nathan.park@gmail.com>
2024-09-06 16:55:58 +0900

    Add PostRecommendRequest and PostRecommendResponse

---
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/request/PostRecommendRequest.kt
@@ -0,0 +1,28 @@
+package com.waddle.gentoo.internal.api.request
+
+import com.waddle.gentoo.internal.api.APPLICATION_JSON
+import com.waddle.gentoo.internal.api.Endpoints
+import com.waddle.gentoo.internal.api.PostRequest
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
+internal class PostRecommendRequest(
+    private val itemId: String,
+    private val userId: String,
+    private val target: String,
+    private val channelId: String = "mobile" // TODO : check if it is fixed string
+) : PostRequest {
+    override val requestBody: RequestBody
+        get() = buildJsonObject {
+            put("itemId", itemId)
+            put("userId", userId)
+            put("target", target)
+            put("channelId", channelId)
+        }.toString().toRequestBody(APPLICATION_JSON)
+    override val url: String
+        get() = Endpoints.RECOMMEND
+    override val headers: Map<String, String>
+        get() = emptyMap()
+}

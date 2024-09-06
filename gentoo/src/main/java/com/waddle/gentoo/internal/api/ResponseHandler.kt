ca3776cfe2e7950a75e2921462f7fd32e0d7ff2a
nathan.park <dev.nathan.park@gmail.com>
2024-09-06 16:56:27 +0900

    Add ResponseHandler

--- gentoo/src/main/java/com/waddle/gentoo/internal/api/ResponseHandler.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/ResponseHandler.kt
@@ -1,7 +1,5 @@
 package com.waddle.gentoo.internal.api
 
-import com.waddle.gentoo.internal.util.GentooResponse
-
 internal fun interface ResponseHandler<T> {
     fun onResponse(response: GentooResponse<T>)
 }

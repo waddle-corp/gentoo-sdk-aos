c4b659efae191c9c60f5a9c81fb4ee7c92c39056
nathan.park <dev.nathan.park@gmail.com>
2024-09-06 23:35:33 +0900

    Add postRecommend test and clean up the code

--- gentoo/src/test/java/com/waddle/gentoo/ApiRequestTest.kt
+++ gentoo/src/test/java/com/waddle/gentoo/ApiRequestTest.kt
@@ -1,48 +1,68 @@
 package com.waddle.gentoo
 
 import com.waddle.gentoo.internal.api.ApiClient
-import com.waddle.gentoo.internal.api.GetRequest
 import com.waddle.gentoo.internal.api.request.AuthRequest
 import com.waddle.gentoo.internal.api.request.GetRecommendRequest
 import com.waddle.gentoo.internal.api.response.AuthResponse
 import com.waddle.gentoo.internal.api.response.GetRecommendResponse
-import com.waddle.gentoo.internal.util.GentooResponse
-import org.junit.Assert
+import com.waddle.gentoo.internal.api.GentooResponse
+import com.waddle.gentoo.internal.api.request.PostRecommendRequest
+import com.waddle.gentoo.internal.api.response.PostRecommendResponse
+import io.kotest.assertions.fail
+import io.kotest.matchers.types.shouldBeTypeOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 internal class ApiRequestsTest {
-    val apiClient = ApiClient("G4J2wPnd643wRoQiK52PO9ZAtaD6YNCAhGlfm1Oc","https://hg5eey52l4.execute-api.ap-northeast-2.amazonaws.com/dev")
+    val apiClient = ApiClient(
+        "G4J2wPnd643wRoQiK52PO9ZAtaD6YNCAhGlfm1Oc",
+        "https://hg5eey52l4.execute-api.ap-northeast-2.amazonaws.com/dev"
+    )
+
     @Test
-    fun auth() {
-        val authRequest = AuthRequest("test", "test")
-        when (val response = apiClient.send(authRequest, AuthResponse.serializer())) {
-            is GentooResponse.Success -> {
-                println("${response.value}")
-            }
-            is GentooResponse.Failure -> {
-                println("${response.e}")
-            }
-        }
+    fun auth() = runBlocking {
+        val authRequest = AuthRequest(testUdid, testAuthCode)
+        val response = apiClient.send(authRequest, AuthResponse.serializer())
+        response.shouldBeTypeOf<GentooResponse.Success<AuthResponse>>()
+        Unit
     }
 
     @Test
-    fun getRecommend() {
-        val authRequest = AuthRequest("test", "test")
+    fun getRecommend() = runBlocking {
+        val authRequest = AuthRequest(testUdid, testAuthCode)
         val userId = when (val response = apiClient.send(authRequest, AuthResponse.serializer())) {
             is GentooResponse.Success -> response.value.body.randomId
-            is GentooResponse.Failure -> throw response.e
+            is GentooResponse.Failure -> fail("AuthRequest should not fail")
         }
 
-        val getRecommendRequest = GetRecommendRequest("3190", userId)
-        when (val response = apiClient.send(getRecommendRequest, GetRecommendResponse.serializer())) {
-            is GentooResponse.Success -> {
-                println("response: ${response.value}")
-            }
-
-            is GentooResponse.Failure -> {
-                println("error: ${response.e}")
-            }
-        }
+        val getRecommendRequest = GetRecommendRequest(testItemId, userId)
+        val response = apiClient.send(getRecommendRequest, GetRecommendResponse.serializer())
+        response.shouldBeTypeOf<GentooResponse.Success<GetRecommendResponse>>()
+        Unit
     }
 
+    @Test
+    fun postRecommend() = runBlocking {
+        val authRequest = AuthRequest(testUdid, testAuthCode)
+        val userId = when (val response = apiClient.send(authRequest, AuthResponse.serializer())) {
+            is GentooResponse.Success -> response.value.body.randomId
+            is GentooResponse.Failure -> fail("AuthRequest should not fail")
+        }
+
+        val thisRequest = PostRecommendRequest(testItemId, userId, "this")
+        var response = apiClient.send(thisRequest, PostRecommendResponse.serializer())
+        response.shouldBeTypeOf<GentooResponse.Success<PostRecommendResponse>>()
+
+        val needsRequest = PostRecommendRequest(testItemId, userId, "needs")
+        response = apiClient.send(needsRequest, PostRecommendResponse.serializer())
+        response.shouldBeTypeOf<GentooResponse.Success<PostRecommendResponse>>()
+        Unit
+    }
+
+    companion object {
+        const val testItemId = "3190"
+        const val testUdid = "test"
+        const val testAuthCode = "test"
+    }
 }
+

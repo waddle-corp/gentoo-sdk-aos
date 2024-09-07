9f5474d7c330cdd44defa003876e362eb39b0634
nathan.park <dev.nathan.park@gmail.com>
2024-09-07 22:43:15 +0900

    Remove host url from buildconfig

--- gentoo/build.gradle.kts
+++ gentoo/build.gradle.kts
@@ -28,14 +28,12 @@ android {
             )
 
             buildConfigField("String", "API_KEY", "\"${System.getenv("GENTOO_RELEASE_API_KEY")}\"")
-            buildConfigField("String", "DAILY_SHOT_BASE_URL", "\"${System.getenv("GENTOO_RELEASE_DAILY_SHOT_BASE_URL")}\"")
         }
 
         debug {
             isMinifyEnabled = false
 
             buildConfigField("String", "API_KEY", "\"${System.getenv("GENTOO_DEBUG_API_KEY")}\"")
-            buildConfigField("String", "DAILY_SHOT_BASE_URL", "\"${System.getenv("GENTOO_DEBUG_DAILY_SHOT_BASE_URL")}\"")
         }
     }
 

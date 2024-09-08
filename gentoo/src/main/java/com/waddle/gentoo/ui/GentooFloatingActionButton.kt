90a7e2b6140923ea3037b75ee910628a07c886f5
nathan.park <dev.nathan.park@gmail.com>
2024-09-08 22:03:06 +0900

    Add GentooFloatingActionButton

---
+++ gentoo/src/main/java/com/waddle/gentoo/ui/GentooFloatingActionButton.kt
@@ -0,0 +1,13 @@
+package com.waddle.gentoo.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+
+class GentooFloatingActionButton(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int
+) : ExtendedFloatingActionButton(context, attrs, defStyleAttr) {
+
+}

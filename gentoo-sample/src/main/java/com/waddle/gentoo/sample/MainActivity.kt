db6208ba3c99cc0b18c229882beec19ff688d0ba
nathan.park <dev.nathan.park@gmail.com>
2024-09-12 23:09:09 +0900

    Implement observing MainViewModel and handover the data to the GentooFloatingActionButton

--- gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
+++ gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.waddle.gentoo.sample
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.waddle.gentoo.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        viewModel.chatUrl.observe(this) {
+            if (it != null) {
+                binding.gentooFloatingActionButton.chatUrl = it
+            }
         }
+
+        viewModel.floatingComment.observe(this) {
+            binding.gentooFloatingActionButton.setFloatingComment(it)
+        }
+
+        viewModel.fetchFloatingComment()
+        viewModel.fetchChatUrl()
     }
 }

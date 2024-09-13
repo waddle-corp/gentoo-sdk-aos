3b60d2ab9b737fdb51b4b5403d1bc610bdef7724
nathan.park <dev.nathan.park@gmail.com>
2024-09-13 23:30:47 +0900

    Remove ViewModel

--- gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
+++ gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
@@ -3,29 +3,15 @@ package com.waddle.gentoo.sample
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.waddle.gentoo.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        viewModel.chatUrl.observe(this) {
-            if (it != null) {
-                binding.gentooFloatingActionButton.chatUrl = it
-            }
-        }
-
-        viewModel.floatingComment.observe(this) {
-            binding.gentooFloatingActionButton.setFloatingComment(it)
-        }
-
-        viewModel.fetchFloatingComment()
-        viewModel.fetchChatUrl()
     }
 }

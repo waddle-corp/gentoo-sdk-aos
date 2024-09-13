91725e621ebda3ffc1466aebbe95a6c73e0634cf
nathan.park <dev.nathan.park@gmail.com>
2024-09-13 23:32:15 +0900

    Add BaseApplication

---
+++ gentoo-sample/src/main/java/com/waddle/gentoo/sample/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.waddle.gentoo.sample
+
+import android.app.Application
+import com.waddle.gentoo.Gentoo
+import com.waddle.gentoo.InitializeParams
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Gentoo.initialize(
+            InitializeParams("test", "test", "dlst")
+        )
+    }
+}

eeeae9917c0f3dc9b4ec6172d594fb8175b1035d
nathan.park <dev.nathan.park@gmail.com>
2024-09-26 00:23:02 +0900

    Refactor Gentoo FAB to use UiState and ViewModel

--- gentoo/src/main/java/com/waddle/gentoo/ui/GentooFloatingActionButton.kt
+++ gentoo/src/main/java/com/waddle/gentoo/ui/GentooFloatingActionButton.kt
@@ -8,20 +8,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.core.view.updateLayoutParams
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.waddle.gentoo.ChatType
 import com.waddle.gentoo.FloatingActionButtonType
-import com.waddle.gentoo.Gentoo
 import com.waddle.gentoo.R
 import com.waddle.gentoo.databinding.ViewGentooFloatingActionButtonBinding
 import com.waddle.gentoo.internal.util.toDp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.waddle.gentoo.viewmodel.GentooViewModel
 
 class GentooFloatingActionButton @JvmOverloads constructor(
     context: Context,
@@ -32,78 +23,52 @@ class GentooFloatingActionButton @JvmOverloads constructor(
         LayoutInflater.from(context)
     )
 
-    private var chatUrl: String? = null
-    private var prepareJob: Job? = null
-
-    var isExpanded: Boolean = false
-        private set
-
-    var itemId: String? = null
+    var chatUrl: String = ""
+    var uiState: GentooViewModel.UiState = GentooViewModel.UiState.Invisible
         set(value) {
             field = value
-            prepare()
-        }
+            when (value) {
+                is GentooViewModel.UiState.Collapsed -> {
+                    binding.root.visibility = VISIBLE
+                    shrink()
+                }
+                is GentooViewModel.UiState.Expanded -> {
+                    binding.root.visibility = VISIBLE
+                    updateComment(value.comment)
+                    expand()
+                }
 
-    var floatingActionButtonType: FloatingActionButtonType = FloatingActionButtonType.HOME
+                GentooViewModel.UiState.Invisible -> binding.root.visibility = GONE
+            }
+        }
 
     init {
-        attrs?.let {
-            val typedArray = context.obtainStyledAttributes(it, R.styleable.GentooFloatingActionButton, defStyleAttr, 0)
-            floatingActionButtonType = when (typedArray.getInt(R.styleable.GentooFloatingActionButton_chat_type, 0)) {
-                0 -> FloatingActionButtonType.HOME
-                1 -> FloatingActionButtonType.DETAIL
-                else -> FloatingActionButtonType.HOME // default
-            }
-
-            typedArray.recycle()
-        }
-
         binding.gentooImageButton.setImageResource(R.drawable.icon_gentoo)
         this.addView(binding.root)
 
         this.binding.root.setOnClickListener {
-            chatUrl?.let {
-                val intent = Intent(context, GentooChatActivity::class.java)
-                intent.putExtra(GentooChatActivity.INTENT_CHAT_URL, it)
-                context.startActivity(intent)
+            val url = this.chatUrl.takeIf { it.isNotEmpty() } ?: return@setOnClickListener
+            val type = when (val state = uiState) {
+                is GentooViewModel.UiState.Expanded -> state.type
+                is GentooViewModel.UiState.Collapsed -> state.type
+                else -> return@setOnClickListener
             }
-        }
+            Log.e("nathan", "url : $url")
 
-        prepare()
-    }
-
-    private fun prepare() {
-        prepareJob?.cancel()
-        prepareJob = CoroutineScope(Dispatchers.IO).launch {
-            try {
-                when (floatingActionButtonType) {
-                    FloatingActionButtonType.HOME -> {
-                        coroutineScope {
-                            this@GentooFloatingActionButton.chatUrl = Gentoo.getHomeChatUrl()
-                        }
-                        updateComment(HOME_FAB_COMMENT)
-                        expand()
-                        delay(AUTO_EXPAND_DELAY)
-                        shrink()
-                    }
-                    FloatingActionButtonType.DETAIL -> {
-                        val itemId = this@GentooFloatingActionButton.itemId ?: return@launch
-                        val floatingComment = Gentoo.fetchFloatingComment(itemId)
-                        updateComment(floatingComment.commentForThis)
-                        this@GentooFloatingActionButton.chatUrl = Gentoo.getDetailChatUrl(itemId, ChatType.THIS, floatingComment.commentForThis)
-                        // TODO shrink and expand
-                    }
+            when (type) {
+                FloatingActionButtonType.HOME -> {
+                    val intent = Intent(context, GentooChatActivity::class.java)
+                    intent.putExtra(GentooChatActivity.INTENT_CHAT_URL, url)
+                    context.startActivity(intent)
                 }
-            } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    binding.root.visibility = GONE
+                FloatingActionButtonType.DETAIL -> {
+                    GentooBottomSheetDialog(context, url) { }.show()
                 }
             }
         }
     }
 
-    private suspend fun expand() = withContext(Dispatchers.Main) {
-        isExpanded = true
+    private fun expand() {
         binding.gentooDescription.visibility = VISIBLE
         binding.root.updateLayoutParams {
             width = ViewGroup.LayoutParams.WRAP_CONTENT // 300.toDp(context)
@@ -116,8 +81,7 @@ class GentooFloatingActionButton @JvmOverloads constructor(
         binding.gentooContainer.setPadding(24.toDp(context), 0, 5.toDp(context), 0)
     }
 
-    private suspend fun shrink() = withContext(Dispatchers.Main) {
-        isExpanded = false
+    private fun shrink() {
         binding.gentooDescription.visibility = GONE
         binding.root.updateLayoutParams {
             width = 55.toDp(context)
@@ -130,12 +94,7 @@ class GentooFloatingActionButton @JvmOverloads constructor(
         binding.gentooContainer.setPadding(0, 0, 0, 0)
     }
 
-    private suspend fun updateComment(text: String) = withContext(Dispatchers.Main) {
+    private fun updateComment(text: String) {
         binding.gentooDescription.text = text
-    }
-
-    companion object {
-        private const val AUTO_EXPAND_DELAY = 3000L
-        private const val HOME_FAB_COMMENT = "술 전문가 젠투에게 술 추천 받아보세요!"
     }
 }

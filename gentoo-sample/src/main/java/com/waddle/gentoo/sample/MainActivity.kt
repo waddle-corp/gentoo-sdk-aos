d1e85fdcd27ad782992bb0fa9894e30859b89aab
nathan.park <dev.nathan.park@gmail.com>
2024-09-26 00:21:40 +0900

    Add example for binding View and ViewModel

--- gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
+++ gentoo-sample/src/main/java/com/waddle/gentoo/sample/MainActivity.kt
@@ -3,15 +3,29 @@ package com.waddle.gentoo.sample
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.waddle.gentoo.Gentoo
 import com.waddle.gentoo.sample.databinding.ActivityMainBinding
+import com.waddle.gentoo.viewmodel.GentooDetailViewModel
+import com.waddle.gentoo.viewmodel.GentooDetailViewModelFactory
+import com.waddle.gentoo.viewmodel.GentooHomeViewModel
 
 class MainActivity : AppCompatActivity() {
+    val viewModel: GentooHomeViewModel by viewModels()
+    val detailViewMode: GentooDetailViewModel by viewModels {
+        GentooDetailViewModelFactory("3190")
+    }
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+        Gentoo.bind(binding.gentooFloatingActionButton, viewModel, lifecycleScope)
+        Gentoo.bind(binding.gentooDetailFloatingActionButton, detailViewMode, lifecycleScope)
     }
 }

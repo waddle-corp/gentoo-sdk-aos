c6f7dfd04223f2716fe289c7b599dbb92e6228f9
nathan.park <dev.nathan.park@gmail.com>
2024-09-26 22:49:11 +0900

    Modify floating button business logic after review.

--- gentoo/src/main/java/com/waddle/gentoo/viewmodel/GentooViewModel.kt
+++ gentoo/src/main/java/com/waddle/gentoo/viewmodel/GentooViewModel.kt
@@ -25,7 +25,7 @@ sealed class GentooViewModel : ViewModel() {
     }
 
     companion object {
-        internal const val AUTO_EXPAND_DELAY = 3000L
+        internal const val AUTO_COLLAPSED_DELAY = 3000L
         internal const val COMMENT_CHANGE_TO_NEEDS_DELAY = 10000L
         internal const val HOME_FAB_COMMENT = "술 전문가 젠투에게 술 추천 받아보세요!"
     }
@@ -37,7 +37,7 @@ class GentooHomeViewModel : GentooViewModel() {
             try {
                 _uiState.emit(UiState.Expanded(FloatingActionButtonType.HOME, HOME_FAB_COMMENT))
                 _chatUrl.emit(Gentoo.getHomeChatUrl())
-                delay(AUTO_EXPAND_DELAY)
+                delay(AUTO_COLLAPSED_DELAY)
                 _uiState.emit(UiState.Collapsed(FloatingActionButtonType.HOME))
             } catch (_: Exception) {}
         }
@@ -53,9 +53,13 @@ class GentooDetailViewModel(
                 val floatingComment = Gentoo.fetchFloatingComment(itemId)
                 _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.THIS, floatingComment.commentForThis))
                 _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, floatingComment.commentForThis))
+                delay(AUTO_COLLAPSED_DELAY)
+                _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
                 delay(COMMENT_CHANGE_TO_NEEDS_DELAY)
                 _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.NEEDS, floatingComment.commentForNeeds))
                 _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, floatingComment.commentForNeeds))
+                delay(AUTO_COLLAPSED_DELAY)
+                _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
             } catch (_: Exception) {}
         }
     }

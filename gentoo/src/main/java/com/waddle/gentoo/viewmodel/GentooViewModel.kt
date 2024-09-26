a6fd6453f324889c5d2707b71216ea8e5c487875
nathan.park <dev.nathan.park@gmail.com>
2024-09-27 00:00:09 +0900

    Improve ViewModel to handle onDismiss and onClick

--- gentoo/src/main/java/com/waddle/gentoo/viewmodel/GentooViewModel.kt
+++ gentoo/src/main/java/com/waddle/gentoo/viewmodel/GentooViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.waddle.gentoo.ChatType
 import com.waddle.gentoo.FloatingActionButtonType
 import com.waddle.gentoo.Gentoo
+import com.waddle.gentoo.internal.api.response.FloatingComment
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,6 +21,10 @@ sealed class GentooViewModel : ViewModel() {
     protected val _chatUrl: MutableStateFlow<String> = MutableStateFlow("")
     val chatUrl: StateFlow<String> = _chatUrl
 
+    open fun onBottomSheetDismissed() {}
+
+    open fun onClicked() {}
+
     sealed class UiState {
         data object Invisible : UiState()
         data class Expanded(val type: FloatingActionButtonType, val comment: String) : UiState()
@@ -27,15 +34,16 @@ sealed class GentooViewModel : ViewModel() {
     companion object {
         internal const val AUTO_COLLAPSED_DELAY = 3000L
         internal const val COMMENT_CHANGE_TO_NEEDS_DELAY = 10000L
-        internal const val HOME_FAB_COMMENT = "술 전문가 젠투에게 술 추천 받아보세요!"
     }
 }
 
 class GentooHomeViewModel : GentooViewModel() {
+    var uiStateJob: Job? = null
     init {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _uiState.emit(UiState.Expanded(FloatingActionButtonType.HOME, HOME_FAB_COMMENT))
+                val floatingComment = Gentoo.fetchFloatingComment(ChatType.DEFAULT, "default")
+                _uiState.emit(UiState.Expanded(FloatingActionButtonType.HOME, floatingComment.message))
                 _chatUrl.emit(Gentoo.getHomeChatUrl())
                 delay(AUTO_COLLAPSED_DELAY)
                 _uiState.emit(UiState.Collapsed(FloatingActionButtonType.HOME))
@@ -47,20 +55,55 @@ class GentooHomeViewModel : GentooViewModel() {
 class GentooDetailViewModel(
     val itemId: String
 ) : GentooViewModel() {
+    private var uiStateJob: Job? = null
+    private var currentChatType: ChatType = ChatType.DEFAULT
+    private var thisFloatingComment: FloatingComment? = null
+    private var needsFloatingComment: FloatingComment? = null
     init {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val floatingComment = Gentoo.fetchFloatingComment(itemId)
-                _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.THIS, floatingComment.commentForThis))
-                _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, floatingComment.commentForThis))
+        updateFloatingComment(false)
+    }
+
+    private fun updateFloatingComment(fromNeeds: Boolean) {
+        uiStateJob = viewModelScope.launch(Dispatchers.IO) {
+            if (!fromNeeds) {
+                currentChatType = ChatType.THIS
+                val thisFloatingComment = thisFloatingComment ?: Gentoo.fetchFloatingComment(ChatType.THIS, itemId).also {
+                    this@GentooDetailViewModel.thisFloatingComment = it
+                }
+
+                _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.THIS, thisFloatingComment.message))
+                _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, thisFloatingComment.message))
                 delay(AUTO_COLLAPSED_DELAY)
+            }
+
+            _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
+            val deferred = async {
+                this@GentooDetailViewModel.needsFloatingComment ?: Gentoo.fetchFloatingComment(ChatType.NEEDS, itemId).also {
+                    this@GentooDetailViewModel.needsFloatingComment = it
+                }
+            }
+            delay(COMMENT_CHANGE_TO_NEEDS_DELAY)
+            currentChatType = ChatType.NEEDS
+            val needsFloatingComment = deferred.await()
+            _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.NEEDS,  needsFloatingComment.message))
+            _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, needsFloatingComment.message))
+            delay(AUTO_COLLAPSED_DELAY)
+            _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
+        }
+    }
+
+    override fun onClicked() {
+        uiStateJob?.cancel()
+    }
+
+    override fun onBottomSheetDismissed() {
+        uiStateJob?.cancel()
+        if (currentChatType == ChatType.THIS) {
+            updateFloatingComment(true)
+        } else {
+            viewModelScope.launch {
                 _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
-                delay(COMMENT_CHANGE_TO_NEEDS_DELAY)
-                _chatUrl.emit(Gentoo.getDetailChatUrl(itemId, ChatType.NEEDS, floatingComment.commentForNeeds))
-                _uiState.emit(UiState.Expanded(FloatingActionButtonType.DETAIL, floatingComment.commentForNeeds))
-                delay(AUTO_COLLAPSED_DELAY)
-                _uiState.emit(UiState.Collapsed(FloatingActionButtonType.DETAIL))
-            } catch (_: Exception) {}
+            }
         }
     }
 }

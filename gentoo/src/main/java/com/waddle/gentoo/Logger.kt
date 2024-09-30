d6f2828288ad6a0736a112f39661e4d3d8ec796d
nathan.park <dev.nathan.park@gmail.com>
2024-09-30 22:38:56 +0900

    Rename LoggerLevel to LogLevel

--- gentoo/src/main/java/com/waddle/gentoo/Logger.kt
+++ gentoo/src/main/java/com/waddle/gentoo/Logger.kt
@@ -5,34 +5,34 @@ import android.util.Log
 internal object Logger {
     private const val TAG = "Gentoo"
 
-    internal var loggerLevel: LoggerLevel = LoggerLevel.NONE
+    internal var loggerLevel: LogLevel = LogLevel.NONE
 
     fun e(message: String) {
-        if (loggerLevel.level >= LoggerLevel.ERROR.level) {
+        if (loggerLevel.level >= LogLevel.ERROR.level) {
             Log.e(TAG, message)
         }
     }
 
     fun w(message: String) {
-        if (loggerLevel.level >= LoggerLevel.WARNING.level) {
+        if (loggerLevel.level >= LogLevel.WARNING.level) {
             Log.w(TAG, message)
         }
     }
 
     fun i(message: String) {
-        if (loggerLevel.level >= LoggerLevel.INFO.level) {
+        if (loggerLevel.level >= LogLevel.INFO.level) {
             Log.i(TAG, message)
         }
     }
 
     fun d(message: String) {
-        if (loggerLevel.level >= LoggerLevel.DEBUG.level) {
+        if (loggerLevel.level >= LogLevel.DEBUG.level) {
             Log.d(TAG, message)
         }
     }
 }
 
-enum class LoggerLevel(internal val level: Int) {
+enum class LogLevel(internal val level: Int) {
     ERROR(0),
     WARNING(1),
     INFO(2),

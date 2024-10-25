9b26803b4545bca7ca48a6e80a939743e2e877d6
nathan.park <dev.nathan.park@gmail.com>
2024-10-25 23:51:35 +0900

    Implement loading gif image once extension

--- gentoo/src/main/java/com/waddle/gentoo/internal/util/Extensions.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/util/Extensions.kt
@@ -1,6 +1,14 @@
 package com.waddle.gentoo.internal.util
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.drawable.MovieDrawable
+import coil.request.CachePolicy
+import coil.request.ImageRequest
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -15,4 +23,39 @@ internal val String.urlEncoded: String
 
 internal fun Int.toDp(context: Context): Int {
     return (this * context.resources.displayMetrics.density).toInt()
+}
+
+internal fun ImageView.loadGif(url: String, onGifAnimationEnded: () -> Unit = {}) {
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            add(GifDecoder.Factory())
+        }
+        .build()
+    val request = ImageRequest.Builder(context)
+        .data(url)
+        .diskCachePolicy(CachePolicy.DISABLED)
+        .memoryCachePolicy(CachePolicy.ENABLED)
+        .target { drawable ->
+            when {
+                drawable is MovieDrawable -> {
+                    drawable.setRepeatCount(0)
+                    drawable.registerAnimationCallback(
+                        object : Animatable2Compat.AnimationCallback() {
+                            override fun onAnimationEnd(drawable: Drawable?) {
+                                onGifAnimationEnded()
+                            }
+                        }
+                    )
+                    this.setImageDrawable(drawable)
+                    drawable.start()
+                }
+
+                else -> {
+                    this.setImageDrawable(drawable)
+                }
+            }
+        }
+        .build()
+
+    imageLoader.enqueue(request)
 }

b886f18fae6e5f3008032b0ee4cbeab2fdb49100
nathan.park <dev.nathan.park@gmail.com>
2024-12-03 22:30:12 +0900

    Implement UserEvent API specification

--- gentoo/src/main/java/com/waddle/gentoo/internal/api/request/UserEventRequest.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/request/UserEventRequest.kt
@@ -3,16 +3,19 @@ package com.waddle.gentoo.internal.api.request
 import com.waddle.gentoo.internal.api.APPLICATION_JSON
 import com.waddle.gentoo.internal.api.Endpoints
 import com.waddle.gentoo.internal.api.PostRequest
+import com.waddle.gentoo.internal.util.Constants
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 
 internal class UserEventRequest(
-    private val userEventCategory: UserEventCategory,
-    private val userId: String,
-    private val clientId: String,
-    private val itemId: String? = null,
+    private val userEvent: UserEvent,
+    private val chatUserId: String,
+    private val partnerId: String,
     private val channelId: String = "Android"
 ) : PostRequest {
     override val url: String
@@ -21,15 +24,44 @@ internal class UserEventRequest(
         get() = emptyMap()
     override val requestBody: RequestBody
         get() = buildJsonObject {
-            put("event_category", userEventCategory.asString)
-            put("visitorId", userId)
-            put("itemId", itemId ?: "general")
-            put("clientId", clientId)
+            put("eventCategory", userEvent.userEventCategory.asString)
+            put("chatUserId", chatUserId)
+            put("partnerId", partnerId)
             put("channelId", channelId)
+
+            when (userEvent) {
+                is UserEvent.PurchaseComplete -> {
+                    put("products", Constants.json.encodeToString(userEvent.products))
+                }
+
+                is UserEvent.AddToCart -> {
+                    put("products", Constants.json.encodeToString(userEvent.products))
+                }
+
+                UserEvent.SdkFloatingClicked -> {}
+                UserEvent.SdkFloatingRendered -> {}
+            }
         }.toString().toRequestBody(APPLICATION_JSON)
 }
 
 internal enum class UserEventCategory(val asString: String) {
     SDK_FLOATING_RENDERED("SDKFloatingRendered"),
-    SDK_FLOATING_CLICKED("SDKFloatingClicked")
+    SDK_FLOATING_CLICKED("SDKFloatingClicked"),
+    PURCHASE_COMPLETE("purchase-complete"),
+    ADD_TO_CART("add-to-cart")
 }
+
+sealed class UserEvent(
+    internal val userEventCategory: UserEventCategory
+) {
+    internal data object SdkFloatingRendered : UserEvent(UserEventCategory.SDK_FLOATING_RENDERED)
+    internal data object SdkFloatingClicked : UserEvent(UserEventCategory.SDK_FLOATING_CLICKED)
+    data class PurchaseComplete(val products: List<Product> = emptyList()) : UserEvent(UserEventCategory.PURCHASE_COMPLETE)
+    data class AddToCart(val products: List<Product> = emptyList()) : UserEvent(UserEventCategory.ADD_TO_CART)
+}
+
+@Serializable
+data class Product(
+    val itemId: String,
+    val quantity: Int
+)

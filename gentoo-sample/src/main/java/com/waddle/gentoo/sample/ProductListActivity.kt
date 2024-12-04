bc5b6c5eea75ff22364dc8116b78f5b99276a997
nathan.park <dev.nathan.park@gmail.com>
2024-12-04 20:46:02 +0900

    Add ProductListActivity

---
+++ gentoo-sample/src/main/java/com/waddle/gentoo/sample/ProductListActivity.kt
@@ -0,0 +1,48 @@
+package com.waddle.gentoo.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.waddle.gentoo.Gentoo
+import com.waddle.gentoo.sample.databinding.ActivityProductListBinding
+import com.waddle.gentoo.viewmodel.GentooProductListViewModel
+
+class ProductListActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProductListBinding
+    val viewModel: GentooProductListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityProductListBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(binding.topBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.topBar.setNavigationIconTint(ContextCompat.getColor(this, R.color.white))
+
+        Gentoo.bind(binding.gentooFloatingActionButton, viewModel, lifecycleScope)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}

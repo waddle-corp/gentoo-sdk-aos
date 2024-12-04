cf383830b9885b4b2e60db95500550d1a90b172a
nathan.park <dev.nathan.park@gmail.com>
2024-12-04 19:40:25 +0900

    Make UserEventResponse empty class because its response is not fixed yet.

--- gentoo/src/main/java/com/waddle/gentoo/internal/api/response/UserEventResponse.kt
+++ gentoo/src/main/java/com/waddle/gentoo/internal/api/response/UserEventResponse.kt
@@ -3,9 +3,4 @@ package com.waddle.gentoo.internal.api.response
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class UserEventResponse(
-    val partnerId: String,
-    val eventType: String,
-    val visitorId: String,
-    val channelId: String
-)
+internal class UserEventResponse
